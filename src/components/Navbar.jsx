317496036c8377948116612ce19513f560b5833a
ericleroy1 <54584831+ericleroy1@users.noreply.github.com>
2021-03-22 18:37:44 +1100

    Navbar styling, responsive

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,28 +1,49 @@
-import React from "react";
+import React, {useState} from "react";
 import { Link } from "react-router-dom";
+import "./Navbar.css";
 
 function Navbar() {
-	return (
-		<div className="navbar">
-			<div className="navbar-container">
-				<Link to="/home">NEWS LOGO</Link>
-				<ul>
-					<li>
-						<Link to="/home">Home</Link>
-					</li>
-					<li>
-						<Link to="/search">Search</Link>
-					</li>
-					<li>
-						<Link to="/mynews">MyNews</Link>
-					</li>
-					<li>
-						<Link to="/logout">Logout</Link>
-					</li>
-				</ul>
-			</div>
-		</div>
-	);
+
+    const [click, setClick] = useState(false);
+    const handleClick = () => setClick(!click);
+    const closeMenu = () => setClick(false);
+
+
+    return (
+        <div className='navbar'>
+            <div className="navbar-container">
+                <Link to="/home" className="navbar-logo" onClick={closeMenu}>
+                        NEWS LOGO <i className='fab fa-typo3'/>
+                </Link>
+
+                <div className='menu-icon' onClick={handleClick}>
+                    <i className={click ? 'fas fa-times' : 'fas fa-bars'} />
+                    </div>
+                    <ul className={click ? 'nav-menu active' : 'nav-menu'}>
+                    <li className='nav-item'>
+                            <Link to='/home' className='nav-links' onClick={closeMenu}>
+                                Home
+                            </Link>
+                        </li>
+                        <li className='nav-item'>
+                            <Link to='/search' className='nav-links' onClick={closeMenu}>
+                                Search
+                            </Link>
+                        </li>
+                        <li className='nav-item'>
+                            <Link to='/mynews' className='nav-links' onClick={closeMenu}>
+                                MyNews
+                            </Link>
+                        </li>
+                        <li className='nav-item'>
+                            <Link to='/logout' className='nav-links' onClick={closeMenu}>
+                                Logout
+                            </Link>
+                        </li>
+                </ul>
+            </div>
+        </div>
+    )
 }
 
 export default Navbar;
--- src/components/NewsItem.jsx
+++ src/components/NewsItem.jsx
@@ -1,19 +1,18 @@
-import React from "react";
+import React, {useState} from "react";
 import { Card, Button } from "react-bootstrap";
 
 function NewsItem(props) {
 
 	return (
 		<Card style={{ width: "90%" }}>
-			<Card.Img variant="top" src={props.image} />
+			<Card.Img variant="top" src={props.image} style={{width: "40%"}}/>
 			<Card.Body>
-				<Card.Title>{props.headline}</Card.Title>
+				<Card.Title>{props.title}</Card.Title>
 				<Card.Text>{props.content}</Card.Text>
 				<a rel="noopener noreferrer" href={props.url} target="_blank">
-				<Button variant="primary">Go somewhere</Button>
+				<Button variant="primary">Go to article</Button>
 				</a>
-
-				
+				<Button>Add to MyNews</Button>
 			</Card.Body>
 		</Card>
 	);
