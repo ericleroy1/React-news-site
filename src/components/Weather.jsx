82d5ab04977e38cce9422061aded253f51077594
ericleroy1 <54584831+ericleroy1@users.noreply.github.com>
2021-03-25 09:41:53 +1100

    added google oAth

---
+++ src/components/Weather.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import "../styles.css";
+
+function Weather() {
+    return (
+        <div>
+            <h3>Sydney</h3>
+            <h4 className="days">Mon </h4>
+            <h4 className="days">Tue </h4>
+            <h4 className="days">Wed </h4>
+            <h4 className="days">Thu </h4>
+            <h4 className="days">Fri</h4>
+        </div>
+    )
+}
+
+export default Weather
