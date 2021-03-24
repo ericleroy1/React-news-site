f06dfb38cf8fea998861598d86ecca72009ab708
ericleroy1 <54584831+ericleroy1@users.noreply.github.com>
2021-03-25 10:21:32 +1100

    functioning node.js backend

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, Component } from "react";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import 'bootstrap/dist/css/bootstrap.min.css';
 
@@ -11,26 +11,46 @@ import MyNewsPage from "./pages/MyNewsPage";
 
 let isLoggedIn = true
 
-function App() {
+class App extends Component {
 
-    return (
-        <Router>
-            <Switch>
-                <Route exact path="/">
-                    {isLoggedIn ? <LandingPage /> : <LoginPage />}
-                </Route>
-                <Route path="/home">
-                    <HomePage/>
-                </Route>
-                <Route path="/search">
-                    <SearchPage/>
-                </Route>
-                <Route path="/mynews">
-                    <MyNewsPage/>
-                </Route>
-            </Switch>
-        </Router>
+    constructor(props) {
+        super(props);
+        this.state = { apiResponse: "" };
+    }
+    
+    callAPI() {
+        fetch("http://localhost:9000/testAPI")
+            .then(res => res.text())
+            .then(res => this.setState({ apiResponse: res }));
+    }
+    
+    componentWillMount() {
+        this.callAPI();
+    }
 
-    )}
+    render() {
+        return (
+        <div>
+            <Router>
+                <Switch>
+                    <Route exact path="/">
+                        {isLoggedIn ? <LandingPage /> : <LoginPage />}
+                    </Route>
+                    <Route path="/home">
+                        <HomePage/>
+                    </Route>
+                    <Route path="/search">
+                        <SearchPage/>
+                    </Route>
+                    <Route path="/mynews">
+                        <MyNewsPage/>
+                    </Route>
+                </Switch>
+            </Router>
+            <p>{this.state.apiResponse}</p>
+        </div>
+        )
+    }
+}
 
 export default App
